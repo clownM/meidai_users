665c0035c182b96b62e218ebe305ad2237764f88
MChen <948849732@qq.com>
2017-11-15 18:45:16 +0800

    Revert "Revert "z""
    This reverts commit 43a1a71443f627615dfed00addc7739c33f53154.

--- js/fswear.js
+++ js/fswear.js
@@ -1,72 +1,67 @@
 // 通过外边框来控制图片的大小可以跟着浏览器窗口变化而变化
-function set_banner_resizer(rim, img, ratio){
-
+function set_banner_resizer(rim, img, ratio) {
     function adjust_banner() {
-        var viewport_height = rim.width()*ratio;
-        rim.height (viewport_height);
+        var viewport_height = rim.width() * ratio;
+        rim.height(viewport_height);
         var img_height = img.height();
         // console.log(rim.height (viewport_height))
-        var offset = rim.offset().top - (img_height-viewport_height)/2;
-        img.offset({top: offset});
+        var offset = rim.offset().top - (img_height - viewport_height) / 2;
+        img.offset({ top: offset });
     }
 
-    adjust_banner();   
+    adjust_banner();
     $(window).resize(adjust_banner);
 }
 
 // 通过点击按钮返回顶部的功能
-function return_top(event){    
-    $(window).scroll(function(){  
-        if($(window).scrollTop()>100){  
-            event.fadeIn(1000);  
-        }else{  
-            event.fadeOut(1000);  
-        }  
-    });  
-
-    event.click(function(){  
-        $('body,html').animate({scrollTop:0},500);  
-        return false;  
-    }); 
-} 
+function return_top(event) {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+            event.fadeIn(1000);
+        } else {
+            event.fadeOut(1000);
+        }
+    });
+    event.click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+}
 
 //设置Cookie
-function setCookie(cname,cvalue,exdays)
-    {
+function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime()+(exdays*24*60*60*1000));
-    var expires = "expires="+d.toGMTString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
-    };
+};
+
 //获得Cookie
-function getCookie(cname){
+function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) 
-      {
-      var c = ca[i].trim();
-      if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    for (var i = 0; i < ca.length; i++)  { 
+        var c = ca[i].trim(); 
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
-    return "";      
+    return "";
 }
-
 
 // 删除Cookie
-function delCookie(name){
+function delCookie(name) {
     var exp = new Date();
     exp.setTime(exp.getTime() - 1);
-    var cval=getCookie(name);
-    if(cval!=null)
-    document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+    var cval = getCookie(name);
+    if (cval != null)
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
 }
-
 
 //检测登录状态
 function checklogin(check_success, check_fail) {
     var _data = {};
     var _user = getCookie("userName");
     var _uuid = getCookie("uuid");
-    if(_uuid != "") {
+    if (_uuid != "") {
         var fsw_obj = {
             action: "query",
             userData: _user,
@@ -79,7 +74,7 @@ function checklogin(check_success, check_fail) {
             data: _data,
             success: function(re) {
                 var re_parsed = JSON.parse(re);
-                if(re_parsed.result == "true") {
+                if (re_parsed.result == "true") {
                     check_success(re);
                     console.log(re)
                 } else {
@@ -90,18 +85,16 @@ function checklogin(check_success, check_fail) {
                 check_fail(re);
             }
         }); 
-    }else check_fail();
+    } else check_fail();
 };
 
-
 /* config 的文本转换成JSON格式 */
-function Config2Json(config)
-{
-    if (! (typeof config === "string")) {
+function Config2Json(config) {
+    if (!(typeof config === "string")) {
         console.log("Config2Json needs input in string type.");
         return;
     }
-    
+
     config = config.split(/\r?\n/);
     var key;
     var value;
@@ -110,19 +103,142 @@ function Config2Json(config)
         pair = config[pair].trim();
         if ("" == pair)
             continue;
-        
+
         var pair_ = pair.split(" ");
         key = pair_[0].trim();
-        if(pair_.length > 1)
+        if (pair_.length > 1)
             value = pair_[1].trim();
         else
             value = "";
-        
+
         if ("" == key)
             continue;
-        
+
         config_json[key] = value;
     }
-    
+
     return config_json;
 }
+
+//将格式RFC2822时间转化时间格式
+function create_time(ordertime) {
+    var _ordertime = ordertime.replace(/-\w*/g, '');
+    var _date = new Date(_ordertime);
+    var order_date = {};
+    order_date._year = _date.getFullYear();
+    order_date._month = _date.getMonth() + 1;
+    order_date._day = _date.getDate();
+    order_date._hour = _date.getHours();
+    order_date._minute = _date.getMinutes();
+    return order_date;
+}
+
+
+// 判定移动端与PC
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+$(document).ready(function() {
+    if (IsPC()) {
+        $('.navbar_pc').load("navbar.html .user_nav", function() {
+
+            /****** 设置头像和用户名 ******/
+            // console.log(getCookie("uuid"))
+            if (getCookie("uuid") != '') {
+                var fsw_obj = {
+                    'action': "query",
+                    'uuid': getCookie("uuid"),
+                    'username': '',
+                    "orders": "",
+                };
+                // console.log(fsw_obj)
+                $.ajax({
+                    url: "/user",
+                    type: 'POST',
+                    data: fsw_obj,
+                    dataType: "json",
+                    // aysnc: false,
+                    success: function(re) {
+                        // console.log(re)
+                        if (re.result == 'true') {
+                            // var userName = JSON.parse(re).username;
+                            var userName = re.username;
+                            // console.log(userName)
+                            $('#username').text(userName);
+                            query_scanphoto(re.orders[0])
+
+
+                            /****** 退出功能按钮 ******/
+                            $('#logout').click(function() {
+                                delCookie('uuid');
+                                window.location.reload();
+                            })
+                        }
+                    },
+                    error: function(er) {
+                        console.log(er)
+                    }
+                }); 
+                function query_scanphoto(orderuuid) {
+                    var order_data = {
+                        "action": "query",
+                        "uuid": orderuuid,
+                        "scanphoto": "",
+                    };
+                    $.ajax({
+                        url: "/order",
+                        type: "post",
+                        data: order_data,
+                        dataType: "json",
+                        aysnc: false,
+                        success: function(data) {
+                            if (data.result == 'true') {
+                                /****** 获取用户头像 ******/
+                                var scanphoto_uuid = data.scanphoto;
+                                if (!scanphoto_uuid) {
+                                    $('.nav-user-portrait').attr('src', 'images/touxiang.png');
+                                } else {
+                                    $('.nav-user-portrait').attr('src', '/data?action=download&type=scanphoto&uuid=' + scanphoto_uuid);
+                                }
+                                /****** 获取用户头像END ******/
+                            }
+                        },
+                        error: function(er) {
+                            console.log(er)
+                        }
+                    });
+                }
+            } else {
+                $('#username').text('请登录');
+                $('#username').css("color", "rgb(85,85,85)");
+                $(".dropdown-toggle").attr("data-toggle", "");
+                $("span.caret").css("display", "none");
+                $('#username').click(function() {
+                    window.location.href = "login.html";
+                    delCookie('uuid');
+                    setCookie("isgoto", "false");
+                    setCookie("checkbox", "false");
+                })
+            }
+
+            /**
+             * 关闭窗口
+             * */
+            // $("#logout").click(function() {
+            //     window.location.href = "login.html";
+            //     delCookie('uuid');
+            //     setCookie("isgoto", "false");
+            //     setCookie("checkbox", "false");
+            // });
+        })
+    }
+});
--- js/order_list.js
+++ js/order_list.js
@@ -1,3 +1,4 @@
+// tabs切换
 function tabsToggle() {
     $("ul.tabs-ul li").click(function() {
         var index = $("ul.tabs-ul li").index(this);
@@ -7,80 +8,156 @@ function tabsToggle() {
         $("div.tabs-contents:eq(" + index + ")").addClass("active");
     });
 }
-$(document).ready(function() {
 
-    tabsToggle();
-    if (getCookie("uuid") != '') {
-        // console.log("cookie-uuid:" + getCookie("uuid"));
-        var fsw_obj = {
-            'action': "query",
-            'uuid': getCookie("uuid"),
-            'username': '',
-        };
-        console.log(fsw_obj);
-        var init_data = {
-            "action": "query",
-            "uuid": fsw_obj.uuid,
-            "phone": "",
-            "username": "",
-            "gender": "",
-            "birthday": "",
-            "orders": "",
-            "lastorder": "",
-        };
-        $.ajax({
-            url: "/user",
-            type: 'POST',
-            data: init_data,
-            dataType: "json",
-            async: false,
-            success: function(data) {
-                if (data.result == 'true') {
+// 获取订单状态
+function get_status(parm1) {
+    var _status;
+    switch (parm1) {
+        case "toconfirm":
+            _status = "扫描未检查";
+            break;
+        case "toscan":
+            _status = "等待扫描";
+            break;
+        case "todeal":
+            _status = "确认";
+            break;
+        default:
+            _status = "未知";
+    }
+    return _status;
+}
 
-                    console.log("data.orders:" + data.orders);
+$(function() {
 
-                    // query_batchorder(data.orders,data.orders.reverse())
-                    // var order_list = {
-                    //     "action": "list",
-                    //     "page": "",
-                    //     "condition": "",
-                    // };
-                    // $.ajax({
-                    //     url: "/order",
-                    //     type: "post",
-                    //     data: order_data,
-                    //     dataType: "json",
-                    //     async: false,
-                    //     success: function(data2) {
-                    //         if (data2.result) {
-                    //             console.log("totalpage:" + data2.totalpage + ";resultedlist:" + data2.resultdlist);
-                    //         }
-                    //     },
-                    //     error: function(erro) {
-                    //         console.log(erro);
-                    //         // $('#userdata_error p').eq(1).css('display','block');
-                    //         // $("#content .article .yy .orderonemonthago").append("<h1 id='remind'>网络错误请重新登录</h1>")
-                    //     },
-                    // });
+    //将订单的uuid全部放入数组中
+    var order_uuid_arr = [];
+
+    var vm = new Vue({
+        el: '#content',
+        data: {
+            ordersArray: []
+        },
+        mounted: function() {
+            this.get_order_info();
+            // DOM经过Vue渲染之后
+            this.$nextTick(() => {
+                tabsToggle();
+                $(".to_order_details").click(function() {
+                    var index = $(".to_order_details").index(this);
+                    console.log(index);
+                    // console.log(order_uuid_arr[index]);
+                    setCookie("orderuuid", order_uuid_arr[index], 1);
+                    // console.log(getCookie("orderuuid"));
+                    window.location.href = "order_details.html";
+                });
+            })
+        },
+        methods: {
+            get_order_info: function() {
+                var ordersArray = [];
+                if (getCookie("uuid") != '') {
+                    console.log("cookie-uuid:" + getCookie("uuid"));
+                    var fsw_obj = {
+                        'action': "query",
+                        'uuid': getCookie("uuid"),
+                        'username': '',
+                    };
+                    //console.log(fsw_obj);
+                    var init_data = {
+                        "action": "query",
+                        "uuid": fsw_obj.uuid,
+                        "phone": "",
+                        "username": "",
+                        "gender": "",
+                        "birthday": "",
+                        "orders": "",
+                        "lastorder": "",
+                    };
+                    $.ajax({
+                        url: "/user",
+                        type: 'POST',
+                        data: init_data,
+                        dataType: "json",
+                        async: false,
+                        success: function(userData) {
+                            if (userData.result == 'true') {
+                                /* console.log(userData.orders.length == 0);
+                                console.log(userData.orders === 0); */
+                                if (!(userData.orders == 0)) {
+                                    var order_data = {
+                                        "action": "query",
+                                        "uuid": JSON.stringify(userData.orders),
+                                        "createdate": "",
+                                        "head": "",
+                                        "config": "",
+                                        "genobj": "",
+                                        "deals": "",
+                                        "station": "",
+                                        "appointmentdate": "",
+                                        "scandate": "",
+                                        "pinfo": "",
+                                        "status": "",
+                                        "preconfig": "",
+                                        "collecteddata": ""
+                                    }
+                                    $.ajax({
+                                        url: "/order",
+                                        type: "post",
+                                        data: order_data,
+                                        dataType: "json",
+                                        async: false,
+                                        success: function(orderData) {
+                                            /* console.log(userData.orders);
+                                            console.log(orderData); */
+                                            var orderuuidarr = userData.orders;
+                                            for (var i = 0; i < orderuuidarr.length; i++) {
+                                                var orderobj = orderData[orderuuidarr[i]];
+                                                var orderUuid = orderuuidarr[i]; //订单uuid
+                                                var thedate = create_time(orderobj.createdate);
+                                                var orderCreateDate = thedate._year + "年" + thedate._month + "月" + thedate._day + "日 " + thedate._hour + ":" + thedate._minute; //订单创建日期
+                                                var station = orderobj.station; //用户选择的扫描站点
+                                                var appointmentdate = orderobj.appointmentdate; //用户预约的扫描时间
+                                                var scandate = orderobj.scandate; //实际的扫描时间
+                                                var status = orderobj.status; //订单的状态
+                                                ordersArray.push({ "orderuuid": orderUuid, "createdate": orderCreateDate, "station": station, "appointmentdate": appointmentdate, "scandate": scandate, "status": status });
+                                                order_uuid_arr.push(orderUuid);
+                                            }
+                                        },
+                                        error: function() {
+
+                                        }
+                                    });
+                                } else {
+                                    $(".tabs").empty();
+                                    $(".tabs").text("当前没有订单！");
+                                    $(".tabs").css({
+                                        "width": "100%",
+                                        "font-size": "40px",
+                                        "text-align": "center",
+                                        "padding-top": "40px"
+                                    });
+                                }
+                            }
+                        },
+                        error: function(re) {
+
+                        }
+                    }); 
+                    this.ordersArray = ordersArray;
+                } else {
+                    // $('.login').html('<a href="login.html"><span class="glyphicon glyphicon-user"></span>登录/注册</a>');
+                    console.log("cookie不存在！");
+                    $(".tabs").empty();
+                    $(".tabs").text("请前往登录！");
+                    $(".tabs").css({
+                        "width": "100%",
+                        "font-size": "40px",
+                        "text-align": "center",
+                        "padding-top": "40px"
+                    });
                 }
             },
-            error: function(re) {
-                console.log(er);
-            }
-        }); 
-
-    } else {
-        // $('.login').html('<a href="login.html"><span class="glyphicon glyphicon-user"></span>登录/注册</a>');
-        console.log("cookie不存在！");
-    }
-
-    function hashCode(str) {
-        var hash = 5381;
-        for (i = 0; i < str.length; i++) {
-            char = str.charCodeAt(i);
-            hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
         }
-        return Math.abs(hash);
-    }
-
-});
+    })
+})
